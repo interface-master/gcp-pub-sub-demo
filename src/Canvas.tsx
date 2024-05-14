380809c27506d44376a5db369b01e0d00e078a12
Michael Vinogradov <interface.master@gmail.com>
2024-05-14 09:44:35 -0400

    📄 scaffolding Canvas and Prediction components

---
+++ src/Canvas.tsx
@@ -0,0 +1,91 @@
+import { useRef, useState } from 'react';
+
+interface CanvasProps {
+    className: string;
+}
+
+interface Point {
+    x: number;
+    y: number;
+}
+
+function Canvas(props: CanvasProps) {
+    const { className } = props;
+
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [lastX, setLastX] = useState(0);
+    const [lastY, setLastY] = useState(0);
+
+    const [pointsList, setPointsList] = useState<Point[]>([]);
+
+    // mouse handlers
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        const ctx = canvasRef.current?.getContext('2d');
+        if (!ctx) return;
+
+        const { offsetX, offsetY } = e.nativeEvent;
+        setIsDrawing(true);
+        setLastX(offsetX);
+        setLastY(offsetY);
+    }
+
+    const handleMouseMove = (e: React.MouseEvent<HTMLCanvasElement>) => {
+        if (!isDrawing) return;
+        const ctx = canvasRef.current?.getContext('2d');
+        if (!ctx) return;
+
+        const { offsetX, offsetY } = e.nativeEvent;
+        drawLine(ctx, offsetX, offsetY);
+        setLastX(offsetX);
+        setLastY(offsetY);
+        setPointsList((old) => [...old, {x: offsetX, y: offsetY}]);
+    }
+
+    const handleMouseUp = () => {
+        setIsDrawing(false);
+        sendDrawingData();
+        clearCanvas();
+        setPointsList([]);
+    }
+
+    // canvas helpers
+
+    const drawLine = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
+        ctx.beginPath();
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+
+    const clearCanvas = () => {
+        const ctx = canvasRef.current?.getContext('2d');
+        if (!ctx || !canvasRef.current) return;
+        ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    }
+
+    // data flow
+
+    const sendDrawingData = () => {
+        console.log('payload size: ', JSON.stringify(pointsList).length, 'sending...', pointsList);
+    }
+
+    // return
+
+    return (
+        <canvas
+            ref={canvasRef}
+            id='drawingArea'
+            className={className} 
+            width='300'
+            height='400'
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+        ></canvas>
+    );
+}
+
+export default Canvas;
---
+++ src/Prediction.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import './Prediction.css';
+
+interface PredictionProps {
+    className: string;
+}
+
+function Prediction(props: PredictionProps) {
+    const { className } = props;
+
+    const [prediction, setPrediction] = useState('...');
+
+    const handlePredictionMessage = async (message: string) => {
+        console.log('Prediction received:', message);
+        setPrediction(message);
+    }
+
+    // TODO: Remove this mock
+    setInterval( () => handlePredictionMessage(
+        String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    ), 5000 );
+
+    return (
+        <div className={className}>
+            <p>I'm guessing it's:</p>
+            <div className='Digit'>{prediction}</div>
+            <div className='Buttons'>
+                <button type='button' className='correct'>Correct</button>
+                <button type='button' className='incorrect'>Incorrect</button>
+            </div>
+        </div>
+    )
+}
+
+export default Prediction;
