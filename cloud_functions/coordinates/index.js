a5b34990bc03ee30f8bef4f73ae65da5b4ea7a1e
Michael Vinogradov <interface.master@gmail.com>
2024-05-14 15:12:09 -0400

    ☁️  updating coordinates function

--- cloud_functions/coordinates/index.js
+++ cloud_functions/coordinates/index.js
@@ -1,7 +1,7 @@
 const functions = require('@google-cloud/functions-framework');
 const { PubSub } = require('@google-cloud/pubsub');
 
-functions.http('coordinates', async (req, res) => {
+functions.http('coordinates', (req, res) => {
   const points = req.body;
 
   const pubsubClient = new PubSub();
@@ -10,8 +10,8 @@ functions.http('coordinates', async (req, res) => {
   try {
     const dataString = JSON.stringify(points);
     const messageBuffer = Buffer.from(dataString, 'utf-8');
-    await topic.publish(messageBuffer);
-    res.status(200).send('Points published successfully!');
+    topic.publishMessage({data: messageBuffer});
+    res.status(200).send('Points published');
   } catch (error) {
     console.error('Error publishing messages:', error);
     res.status(500).send('Error publishing points!');
