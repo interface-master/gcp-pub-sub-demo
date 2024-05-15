6c7409f0a2b020be0c0c3f4bc8982fc2cfc39d9c
Michael Vinogradov <interface.master@gmail.com>
2024-05-14 23:55:59 -0400

    ☁️  package updates

--- cloud_functions/coordinates/index.js
+++ cloud_functions/coordinates/index.js
@@ -2,18 +2,20 @@ const functions = require('@google-cloud/functions-framework');
 const { PubSub } = require('@google-cloud/pubsub');
 
 functions.http('coordinates', (req, res) => {
-  const points = req.body;
+  if (req.method === 'POST') {
+    const points = req.body;
 
-  const pubsubClient = new PubSub();
-  const topic = pubsubClient.topic('coordinates');
-  
-  try {
-    const dataString = JSON.stringify(points);
-    const messageBuffer = Buffer.from(dataString, 'utf-8');
-    topic.publishMessage({data: messageBuffer});
-    res.status(200).send('Points published');
-  } catch (error) {
-    console.error('Error publishing messages:', error);
-    res.status(500).send('Error publishing points!');
+    const pubsubClient = new PubSub();
+    const topic = pubsubClient.topic('coordinates');
+    
+    try {
+      const dataString = JSON.stringify(points);
+      const messageBuffer = Buffer.from(dataString, 'utf-8');
+      topic.publishMessage({data: messageBuffer});
+      res.status(200).send('Points published');
+    } catch (error) {
+      console.error('Error publishing messages:', error);
+      res.status(500).send('Error publishing points!');
+    }
   }
 });
